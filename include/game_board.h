6685de88cededb7bd160100bbaae1d95c138e3ac
jevon-23 <jevon32101@gmail.com>
2022-11-12 14:50:54 -0800

    draw 1, fix stock pile

--- include/game_board.h
+++ include/game_board.h
@@ -4,19 +4,22 @@
 #define GAME_BOARD_H
 
 #define MAX_PILE_LEN SUIT_SIZE
+#define MAX_STOCK_LEN 24 // (52 - (7 + 6 + 5 + 4 + 3 + 2 + 1))
 
-enum pile_type { DECK_PILE, TABLE_PILE, FOUNDATION_PILE };
+enum pile_type { DECK_PILE, TABLE_PILE, FOUNDATION_PILE , STOCK_PILE };
 
 typedef struct pile {
   card *cards;         // Pile of the cards that we have
   int len;             // The current length of the pile
   int max_len;         // The max length this pile can reach
+  int index;           // What number card we are at (used 4 stock)
   enum pile_type type; // The type of pile
 } pile;
 
 typedef struct game_board {
   pile table[7];         // cards not on the table
   pile foundation[4];    // piles for foundation 2 win
+  pile *stock;        // Cards that have not been used in foundation
   pile *deck;            // All 52 cards in the deck
   bool used[52];         // True if card has been used
 } game_board;
--- src/game_board/game_board.c
+++ src/game_board/game_board.c
@@ -14,6 +14,9 @@
  * goes
  */
 
+/**************************/
+/* Printing functionality */
+/**************************/
 void print_pile(pile *p, enum pile_type type) {
   printf("pile_len = %d\n", p->len);
   if (p->len == 0) {
@@ -24,8 +27,30 @@ void print_pile(pile *p, enum pile_type type) {
   }
 }
 
+bool check_finished(bool *finished, int len) {
+    for (int j = 0; j < len; j++) {
+      if (!finished[j])
+          return false;
+    }
+    return true;
+}
+
+void print_stock_deck(game_board *gb) {
+  int printed = 0;
+
+  printf("printing left over deck\n");
+  for (int i = 0; i < gb->stock->len; i++) {
+    card *c = get_card(gb->stock, i);
+    printf("%c:%c|", 
+            card_value_to_str(c->val), card_suit_to_str(c->suit));
+    printed++;
+    if (printed % 5 == 0)
+      printf("\n");
+  }
+    printf("\n");
+}
+
 void print_foundational_piles(game_board *gb) {
-  bool done = true;
   bool finished[4] = {false, false, false, false};
 
   // foundational piles
@@ -43,20 +68,13 @@ void print_foundational_piles(game_board *gb) {
     }
     printf("\n");
 
-    for (int j = 0; j < 4; j++) {
-      if (done && !finished[j])
-        done = false;
-    }
-    if (done)
+    if (check_finished(finished, 4))
       break;
-    else
-      done = true;
   }
 }
 
 void print_table(game_board *gb) {
-  bool done = true;
-  bool finished[8] = {false, false, false, false, false, false, false, false};
+  bool finished[8] = {true, false, false, false, false, false, false, false};
 
   printf("printing out the current table of the game\n");
   for (int i = 0; i < SUIT_SIZE; i++) {
@@ -72,28 +90,27 @@ void print_table(game_board *gb) {
         continue;
       }
       card *c = get_card(p, i);
-      printf("%c:%c|",  card_value_to_str(c->val), card_suit_to_str(c->suit));
+      printf("%c:%c|",
+              card_value_to_str(c->val),
+              card_suit_to_str(c->suit));
     }
     printf("\n");
 
-    for (int j = 1; j < 8; j++) {
-      if (done && !finished[j])
-        done = false;
-    }
-    if (done)
-      break;
-    else
-      done = true;
+    if (check_finished(finished, 8)) 
+        break;
   }
-
 }
 
 void print_game_board(game_board *gb) {
   printf("Foundation: \n");
   print_foundational_piles(gb);
   print_table(gb);
+  print_stock_deck(gb);
 }
 
+/**********************/
+/* Pile functionality */
+/**********************/
 card *get_card(pile *p, int index) {
   if (index >= p->len)
     return NULL;
@@ -101,13 +118,13 @@ card *get_card(pile *p, int index) {
   return (p->cards + p->max_len - p->len + index);
 }
 
-/* Insert the card on to the back of the pile */
+/* Insert the card on to the front of the pile */
 void insert_pile(pile **p, card *c) {
   *((*p)->cards + (*p)->max_len - (*p)->len - 1) = *c;
   (*p)->len++;
 }
 
-int rand_no() { return rand() % DECK_SIZE; }
+int rand_no(max) { return rand() % max; }
 
 /* Pile_no between 1-7 */
 pile *make_pile(game_board *gb, enum pile_type type, int pile_no) {
@@ -141,23 +158,57 @@ pile *make_pile(game_board *gb, enum pile_type type, int pile_no) {
     p->max_len = MAX_PILE_LEN;
     p->cards = (card *)malloc(sizeof(card) * MAX_PILE_LEN);
 
+    srand(time(NULL));
     for (int pile_index = 0; pile_index < pile_no; pile_index++) {
       // Get a random card that is not on the board yet
-      int rand_card = rand_no();
+      int rand_card = rand_no(DECK_SIZE);
       while (gb->used[rand_card]) {
-        rand_card = rand_no();
+        rand_card = rand_no(DECK_SIZE);
       }
 
       /* Update pile, and our used array */
       insert_pile(&p, (gb->deck->cards + rand_card));
-      // *(p->cards + pile_index) = *(gb->deck->cards + rand_card);
       gb->used[rand_card] = true;
     }
     break;
+
+  case STOCK_PILE:;
+    int *indicies = (int *)malloc(sizeof(int) * MAX_STOCK_LEN);
+    int i_len = 0;
+    p->cards = (card *)malloc(sizeof(card) * MAX_STOCK_LEN);
+    p->max_len = MAX_STOCK_LEN;
+    /* Get indicies of unused cards */
+    for (int i = 0; i < 52; i++) {
+        if (!gb->used[i]) {
+            *(indicies + (i_len++)) = i;
+        }
+    }
+
+    /* Shuffle the unused inidices and put the cards in unused */
+    bool *check = (bool *)calloc(MAX_STOCK_LEN, sizeof(bool));
+    for (int i = 0; i < i_len; i++) {
+        int rand_card = rand_no(MAX_STOCK_LEN);
+        while (check[rand_card]) {
+            rand_card = rand_no(MAX_STOCK_LEN);
+        }
+        card *c = (gb->deck->cards + *(indicies + rand_card));
+        insert_pile(&p, c);
+    }
+
+    free(indicies);
+    break;
+
+    default:
+      error("Invalid pile passsed in to make_pile", -1);
+      exit(-1);
   }
   return p;
 }
 
+
+/***************************/
+/* Gameboard functionality */
+/***************************/
 game_board *init_game_board() {
   game_board *gb = (game_board *)malloc(sizeof(game_board));
   gb->deck = make_pile(gb, DECK_PILE, 0);
@@ -180,16 +231,37 @@ game_board *init_game_board() {
     memcpy((gb->table + pile_no), p, sizeof(pile));
   }
   printf("Finsihed generating starting piles\n");
+
+  printf("Generating stock pile\n");
+  gb->stock = make_pile(gb, STOCK_PILE, 0);
+  printf("Finished generating stock pile\n");
+
+  
+
+  /* Generate the stock cards pile */
   return gb;
+}
+
+card *draw_one(game_board *gb) {
+    card *c = get_card(gb->stock, gb->stock->index);
+    if (c == NULL) {
+        gb->stock->index = 0;
+        c = get_card(gb->stock, gb->stock->index);
+        if (c == NULL) {
+            error("Couldn't get card out of deck or no more cards left", -1);
+        }
+    }
+    gb->stock->index++;
+    return c;
 }
 
 void test() {
   game_board *gb = init_game_board();
-
-  // print_card(get_card(gb->deck, 0));
-  // print_pile(gb->deck, DECK_PILE);
-  // print_pile((gb->foundation + 0));
-  // print_foundational_piles(gb);
-  // print_table(gb);
   print_game_board(gb);
+
+  // for (int i = 0; i < 25; i++) {
+
+  //   card *c = draw_one(gb);
+  //   print_card(c);
+  // }
 }
